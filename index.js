cb51a58021ab9f3348e08a333899f8f9645c24db
tzmanics <tzmanics@gmail.com>
2020-10-07 15:30:17 -0400

    adds things

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 module.exports = {
   onPreBuild: () => {
     console.log('Hellooo PreBuild ヽ(^。^)丿!')
+  },
+  onBuild: ({ constants, inputs }) => {
+    console.log('onPostBuild: I run _after_ build commands are executed ✅')
+    console.log('Here are some other things I know thanks to constants:')
+    console.log(constants)
+    console.log(`Oh, and your database URL is: ${inputs.databaseUrl}.`)
+  },
+  onPostBuild: ({ utils }) => {
+    console.log('onBuild: I run on build')
+    try {
+      throw new Error('This is the error 🚨')
+    } catch (error) {
+      utils.build.cancelBuild(`This will cancel the build ${error}.`)
+    }
   }
 }
